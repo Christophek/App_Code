fe4bff0f1bba748df0d9583f7983abf043bbbbd1
Christophek <christophejpkk@gmail.com>
2015-08-24 14:56:54 +0200

    VinoPro.cs

---
+++ VinoPro.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Egenskaber for VinoPro
+/// </summary>
+public class VinoPro
+{
+    public int ID
+    {
+        get;
+        set;
+    }
+
+
+
+    public string Tekst
+    {
+        get;
+        set;
+    }
+}
